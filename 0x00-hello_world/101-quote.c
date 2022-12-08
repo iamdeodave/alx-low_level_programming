5decc36dd712ef52d8b14d279635808abfbf375a
iamdeodave <david.eyong.otu@gmail.com>
2022-12-08 15:24:34 -0800

    Another correction to Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 #include <unistd.h>
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }

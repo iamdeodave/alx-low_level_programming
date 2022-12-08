7813958e67e68fc12121daff55db531527e45ce3
iamdeodave <david.eyong.otu@gmail.com>
2022-12-08 15:31:09 -0800

    new task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
+/**
+ * main - Entry Point
+ * Return:1
+ */
 #include <stdio.h>
 #include <unistd.h>
-/**                                                                                           
-* main - Entry point                                                                         
-* Return: 1
-*/
+                                                                     
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);

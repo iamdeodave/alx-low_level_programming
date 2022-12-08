eb55bdadc4cd0343b54164a8a07d14ebeb6ae5cb
iamdeodave <david.eyong.otu@gmail.com>
2022-12-08 15:57:24 -0800

    new edit on Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
  * Return:1
  */
 #include <stdio.h>
-#include <unistd.h>                                                                  
+#include <unistd.h>
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
